1b3b629c1dcf41c28cf2191c06bd12fdbe839bae
Paulo Rogério <80416920+RealMeloLima@users.noreply.github.com>
2024-09-26 16:10:52 -0300

    Change

--- Calculo_Fatorial.c
+++ Calculo_Fatorial.c
@@ -3,15 +3,15 @@
 int main() {
     int number;
     unsigned long long factorial = 1;
-    printf("Enter an integer: ");
+    printf("Insira um número inteiro: ");
     scanf("%d", &number);
     if (number < 0) {
-        printf("Factorial is not defined for negative numbers.\n");
+        printf("Fatoriais não são definidos para números negativos.\n");
         return 1;
     }
     for(int i = 1; i <= number; ++i){
         factorial *= i;
     }
-    printf("Factorial of %d = %llu\n", number, factorial);
+    printf("Fatorial de %d = %llu\n", number, factorial);
     return 0;
 }
